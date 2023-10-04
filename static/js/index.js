d15b070b925d244a242327f41df584cbdba96e51
vfomenko <vladislav.fomenko.2014@gmail.com>
2023-10-04 13:54:53 +0300

    Add point

--- static/js/index.js
+++ static/js/index.js
@@ -153,7 +153,7 @@ const errorMessage = (inputType, message) => {
  */
 
 const validateEmail = (email) => {
-    let re = new RegExp(/^[A-Za-z0-9_!-]+@[A-Za-z0-9.-]+$/gm);
+    let re = new RegExp(/^[A-Za-z0-9_.-]+@[A-Za-z0-9.-]+$/gm);
     return re.test(email);
 };
 
