81fe7d7104a37d7014e12ac7a75f5198bd80add5
vfomenko <vladislav.fomenko.2014@gmail.com>
2023-11-25 12:45:36 +0300

    Done components for questionnaire

---
+++ src/components/Questionnaire/atomic/starButton/starButton.js
@@ -0,0 +1,16 @@
+import Component from '../../../core/basicComponent';
+import template from './starButton.hbs';
+/**
+ * слои-обертки
+ * @class
+ * @param {HTMLElement} parent - Родительский элемент, в который будет вставлен компонент.
+ * @param {Object} config - Объект с конфигурацией компонента.
+ */
+export default class StarButton extends Component {
+    /**
+     * Рендерит компонент в DOM
+     */
+    render() {
+        return template({ rating: this.config });
+    }
+}
