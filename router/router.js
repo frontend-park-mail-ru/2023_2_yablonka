4a563af0467b6d65939dbab82efbc7e2a8a6ebf3
Glibusss <expansifff@gmail.com>
2023-11-25 14:21:08 +0300

    Добавил questionnaire страницу для iframe

--- router/router.js
+++ router/router.js
@@ -13,11 +13,15 @@ const createPath = (page) => path.resolve(__dirname, '../dist', `${page}.html`);
 
 router.use(express.static(path.resolve(__dirname, '../dist')));
 
-/**
- * Проверяет, можно ли переходить по такому адресу
- * @param {string} view - название страницы
- * @returns {boolean} - true, если разрешено, false, если нет
- */
+router.get('/questionnaire', (req, res) => {
+    const filePath = createPath('questionnaire.html');
+    if (!fs.existsSync(filePath)) {
+        res.status(500).send('500. Internal server Error');
+        return;
+    }
+
+    res.sendFile(filePath);
+});
 
 router.get('*', (req, res) => {
     const filePath = createPath('index');
