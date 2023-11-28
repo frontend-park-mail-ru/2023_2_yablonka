d0076fc288daebc5d6c7066bc7321d78e350a8b4
Glibusss <expansifff@gmail.com>
2023-11-28 22:52:44 +0300

    Убрал cleanup, добавил опцию в конфиг

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].[contenthash].js',
         publicPath: './',
+        clean: true,
     },
     devServer: {
         allowedHosts: 'all',
