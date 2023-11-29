7e19bef104d1bf1c113c08e6c1adbf1b3e813f00
vfomenko <vladislav.fomenko.2014@gmail.com>
2023-11-29 12:38:43 +0300

    Fix add date

--- src/modules/regExp.js
+++ src/modules/regExp.js
@@ -4,7 +4,7 @@ export const navigationPagesHrefRegExp = /^\/[a-zA-Z0-9]+$/;
 
 export const boardHrefRegExp = /^\/workspace_\w+_board_\w+(_card_\w+)?$/;
 
-export const validateObjectNameRegExp = /^[a-zA-Z0-9!?#^_а-яА-Я][a-zA-Z0-9!?#^   _а-яА-Я ]+/;
+export const validateObjectNameRegExp = /^[a-zA-Z0-9!?#^:;'"\\*_а-яА-ЯёЁ][a-zA-Z0-9!?#^:;'"\\*_а-яА-ЯёЁ ] +$/;
 
 export const validatePasswordRegExp = /^[a-zA-Z0-9!?#^$&_]{8,}$/;
 
