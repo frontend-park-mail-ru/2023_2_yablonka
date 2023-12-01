fe8b99c6374b8df89eecadc128ee3ba8872eaf65
Glibuss <expansifff@gmail.com>
2023-12-01 04:55:02 +0300

    fix

--- src/storages/workspaceStorage.js
+++ src/storages/workspaceStorage.js
@@ -139,7 +139,7 @@ class WorkspaceStorage extends BaseStorage {
             this.storage.set(this.workspaceModel.users, body.body.users);
             this.storage.set(this.workspaceModel.comments, body.body.comments);
             this.storage.set(this.workspaceModel.checklists, body.body.checklists);
-            this.storage.set(this.workspaceModel.items, body.body.checklist_items);
+            this.storage.set(this.workspaceModel.items, body.body.tems);
         } else if (status === 403) {
             emitter.trigger('noaccess');
         }
