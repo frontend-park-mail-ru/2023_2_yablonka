bb1b95e6675901041be1cd0775683d3bd4341c30
vfomenko <vladislav.fomenko.2014@gmail.com>
2023-12-06 00:21:56 +0300

    Upgrade nginx config

--- tools/nginx_start.bash
+++ tools/nginx_start.bash
@@ -1,4 +1,5 @@
-sudo cp /home/vfomenko/Programming/Technopark/Frontend/ProjectTrello/front-end/2023_2_yablonka/nginx/tabula.conf /etc/nginx/sites-available/
+cd .. && cd ./nginx
+sudo cp ./tabula.conf /etc/nginx/sites-available/
 sudo rm /etc/nginx/sites-enabled/tabula.conf
 sudo ln -s /etc/nginx/sites-available/tabula.conf /etc/nginx/sites-enabled/
 sudo systemctl restart nginx
