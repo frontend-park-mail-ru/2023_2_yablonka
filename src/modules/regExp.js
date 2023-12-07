e5dcbe1d0eb09fe31307f45d55530e28335865c0
vfomenko <vladislav.fomenko.2014@gmail.com>
2023-12-07 19:01:01 +0300

    fix regexp

--- src/modules/regExp.js
+++ src/modules/regExp.js
@@ -4,9 +4,9 @@ export const navigationPagesHrefRegExp = /^\/[a-zA-Z0-9]+$/;
 
 export const boardHrefRegExp = /^\/workspace_\w+_board_\w+(_card_\w+)?$/;
 
-export const validateObjectNameRegExp = /^[a-zA-Z0-9!?#^:;'"\\*_а-яА-ЯёЁ][a-zA-Z0-9!?#^:;'"\\*_а-яА-ЯёЁ ]+$/;
+export const validateObjectNameRegExp = /^[a-zA-Z0-9@!?#^:;'"\\*_а-яА-ЯёЁ][a-zA-Z0-9@!?#^:;'"\\*_а-яА-ЯёЁ ]*$/;
 
-export const validatePasswordRegExp = /^[a-zA-Z0-9!?#^$&_]{8,}$/;
+export const validatePasswordRegExp = /^[a-zA-Z0-9@!?#^$&_]{8,}$/;
 
 export const validateEmailRegExp = /^\w+@\w+(\.)(\w+)$/;
 
