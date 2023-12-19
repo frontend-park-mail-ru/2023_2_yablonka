fb597906bcdcd665dff2d1d2e85b88c847fbfcfb
vfomenko <vladislav.fomenko.2014@gmail.com>
2023-12-19 04:28:04 +0300

    fix file_path in request

--- src/components/Card/popups/addFile/addFile.js
+++ src/components/Card/popups/addFile/addFile.js
@@ -194,7 +194,7 @@ export default class AddFile extends Component {
                 actionDeleteFile({
                     file_path: file
                         .querySelector('.card-file-download')
-                        .href.replace(window.origin, ''),
+                        .href.replace(`${window.origin}/`, ''),
                     original_name: file.querySelector('.card-information__filename').textContent,
                     task_id: parseInt(e.target.closest('#card').dataset.card, 10),
                 }),
