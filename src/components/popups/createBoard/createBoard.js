a9bf3d392621eae2d266c9266f305aa7f3ff4123
Gleb Makarenko <113942267+Glibusss@users.noreply.github.com>
2024-02-20 14:31:46 +0300

    Board name hotfix

--- src/components/popups/createBoard/createBoard.js
+++ src/components/popups/createBoard/createBoard.js
@@ -165,7 +165,7 @@ export default class CreateBoard extends Component {
             NotificationMessage.showNotification(boardName, true, true, {
                 fontSize: 14,
                 fontWeight: 200,
-                text: 'Название доски должно содержать лишь символы латиницы, кириллицы, цифры и спецсимволы',
+                text: `Название доски может содержать лишь буквы кириллицы и латиницы, цифры, спецсимволы $@!?#^:;%'"\\*_ и быть не пустым`,
             });
         }
     };
